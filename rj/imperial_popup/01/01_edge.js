9d6e913d5bd3569efc84a611795193a99e301ee6
Oleg <vashukov@gmail.com>
2015-09-29 16:46:41 +0300

    we
    we

---
+++ rj/imperial_popup/01/01_edge.js
@@ -0,0 +1,225 @@
+/*jslint */
+/*global AdobeEdge: false, window: false, document: false, console:false, alert: false */
+(function (compId) {
+
+    "use strict";
+    var im='images/',
+        aud='media/',
+        vid='media/',
+        js='js/',
+        fonts = {
+        },
+        opts = {
+            'gAudioPreloadPreference': 'auto',
+            'gVideoPreloadPreference': 'auto'
+        },
+        resources = [
+        ],
+        scripts = [
+        ],
+        symbols = {
+            "stage": {
+                version: "6.0.0",
+                minimumCompatibleVersion: "5.0.0",
+                build: "6.0.0.400",
+                scaleToFit: "none",
+                centerStage: "vertical",
+                resizeInstances: false,
+                content: {
+                    dom: [
+                        {
+                            id: 'bg',
+                            type: 'image',
+                            rect: ['0px', '0px', '1000px', '725px', 'auto', 'auto'],
+                            fill: ["rgba(0,0,0,0)",im+"bg.png",'0px','0px']
+                        },
+                        {
+                            id: 'shadow',
+                            type: 'image',
+                            rect: ['0px', '0px', '1000px', '725px', 'auto', 'auto'],
+                            opacity: '0',
+                            fill: ["rgba(0,0,0,0)",im+"shadow.png",'0px','0px']
+                        },
+                        {
+                            id: 'window',
+                            type: 'image',
+                            rect: ['48px', '36px', '904px', '666px', 'auto', 'auto'],
+                            opacity: '0',
+                            fill: ["rgba(0,0,0,0)",im+"window.png",'0px','0px'],
+                            transform: [[],[],[],['0','0']]
+                        },
+                        {
+                            id: 'content',
+                            type: 'image',
+                            rect: ['168px', '95px', '641px', '470px', 'auto', 'auto'],
+                            opacity: '0',
+                            fill: ["rgba(0,0,0,0)",im+"content.png",'0px','0px']
+                        },
+                        {
+                            id: 'r',
+                            type: 'image',
+                            rect: ['0px', '0px', '1000px', '725px', 'auto', 'auto'],
+                            opacity: '0',
+                            fill: ["rgba(0,0,0,0)",im+"r.png",'0px','0px']
+                        },
+                        {
+                            id: 'g',
+                            type: 'image',
+                            rect: ['0px', '0px', '1000px', '725px', 'auto', 'auto'],
+                            opacity: '0',
+                            fill: ["rgba(0,0,0,0)",im+"g.png",'0px','0px']
+                        },
+                        {
+                            id: 'b',
+                            type: 'image',
+                            rect: ['0px', '0px', '1000px', '725px', 'auto', 'auto'],
+                            opacity: '0',
+                            fill: ["rgba(0,0,0,0)",im+"b.png",'0px','0px']
+                        },
+                        {
+                            id: 'Rectangle',
+                            type: 'rect',
+                            rect: ['426px', '559px', '146px', '44px', 'auto', 'auto'],
+                            opacity: '0',
+                            fill: ["rgba(192,192,192,1)"],
+                            stroke: [0,"rgba(0,0,0,1)","none"]
+                        },
+                        {
+                            id: 'menu2',
+                            type: 'image',
+                            rect: ['160px', '0px', '840px', '56px', 'auto', 'auto'],
+                            fill: ["rgba(0,0,0,0)",im+"menu.png",'0px','0px']
+                        }
+                    ],
+                    style: {
+                        '${Stage}': {
+                            isStage: true,
+                            rect: [undefined, undefined, '100%', '100%'],
+                            overflow: 'hidden',
+                            fill: ["rgba(0,0,0,1.00)"]
+                        }
+                    }
+                },
+                timeline: {
+                    duration: 1500,
+                    autoPlay: false,
+                    data: [
+                        [
+                            "eid45",
+                            "opacity",
+                            1000,
+                            250,
+                            "easeInOutBounce",
+                            "${b}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid50",
+                            "opacity",
+                            1250,
+                            250,
+                            "easeInOutBounce",
+                            "${b}",
+                            '1',
+                            '0'
+                        ],
+                        [
+                            "eid44",
+                            "opacity",
+                            750,
+                            250,
+                            "easeInOutBounce",
+                            "${g}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid49",
+                            "opacity",
+                            1000,
+                            250,
+                            "easeInOutBounce",
+                            "${g}",
+                            '1',
+                            '0'
+                        ],
+                        [
+                            "eid28",
+                            "scaleX",
+                            250,
+                            250,
+                            "easeInOutBack",
+                            "${window}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid42",
+                            "opacity",
+                            500,
+                            1000,
+                            "linear",
+                            "${content}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid30",
+                            "scaleY",
+                            250,
+                            250,
+                            "easeInOutBack",
+                            "${window}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid8",
+                            "opacity",
+                            0,
+                            250,
+                            "linear",
+                            "${shadow}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid6",
+                            "opacity",
+                            250,
+                            250,
+                            "easeInOutBack",
+                            "${window}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid46",
+                            "opacity",
+                            500,
+                            250,
+                            "easeInOutBounce",
+                            "${r}",
+                            '0',
+                            '1'
+                        ],
+                        [
+                            "eid48",
+                            "opacity",
+                            750,
+                            250,
+                            "easeInOutBounce",
+                            "${r}",
+                            '1',
+                            '0'
+                        ]
+                    ]
+                }
+            }
+        };
+
+    AdobeEdge.registerCompositionDefn(compId, symbols, fonts, scripts, resources, opts);
+
+    if (!window.edge_authoring_mode) AdobeEdge.getComposition(compId).load("01_edgeActions.js");
+})("EDGE-81113043");
---
+++ rj/imperial_popup/01/01_edgeActions.js
@@ -0,0 +1,37 @@
+/***********************
+* Adobe Edge Animate Composition Actions
+*
+* Edit this file with caution, being careful to preserve 
+* function signatures and comments starting with 'Edge' to maintain the 
+* ability to interact with these actions from within Adobe Edge Animate
+*
+***********************/
+(function($, Edge, compId){
+var Composition = Edge.Composition, Symbol = Edge.Symbol; // aliases for commonly used Edge classes
+
+   //Edge symbol: 'stage'
+   (function(symbolName) {
+      
+      
+      Symbol.bindElementAction(compId, symbolName, "${b}", "click", function(sym, e) {
+         // insert code for mouse click here
+         sym.play();
+         
+         
+         
+
+      });
+      //Edge binding end
+
+      Symbol.bindElementAction(compId, symbolName, "${Rectangle}", "click", function(sym, e) {
+         // insert code for mouse click here
+         sym.playReverse();
+         
+
+      });
+      //Edge binding end
+
+   })("stage");
+   //Edge symbol end:'stage'
+
+})(window.jQuery || AdobeEdge.$, AdobeEdge, "EDGE-81113043");
